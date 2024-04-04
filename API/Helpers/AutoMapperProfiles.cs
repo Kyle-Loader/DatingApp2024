48f9ef23b6dfb912856bd58a591a64eabf1714c9
Kyle-Loader <kyleloaders@gmail.com>
2024-04-04 12:09:36 +0200

    End of section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        //The purpose of a automapper is to map object properties by name and performs methods that have a Get in front.
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
 //CSIP6833 (2024)
 //K. Loader (2019570346)
 
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -20,5 +22,20 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
