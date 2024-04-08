ee43977d3580c45e6ef59b3120b4ad01416fd6cb
Kyle-Loader <kyleloaders@gmail.com>
2024-04-08 12:23:56 +0200

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -51,7 +51,7 @@ public class AccountController : BaseApiController
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login (LoginDto loginDto)
     {
-        var user = await _context.Users.FirstOrDefaultAsync(user => user.UserName == loginDto.Username);
+        var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(user => user.UserName == loginDto.Username);
 
         if(user == null) return Unauthorized("Invalid username.");
 
@@ -70,7 +70,8 @@ public class AccountController : BaseApiController
         return new UserDto
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
     }
 
